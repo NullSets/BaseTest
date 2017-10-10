b76112db14adc3fde7b82356c8970c8e7237e0ad
dwl5897080 <609734236@qq.com>
2017-10-10 20:30:14 +0800

    learn network

--- www.py
+++ www.py
@@ -1,10 +1,8 @@
 
 
-
-
 from urllib import request
-import re
 
+import re
 p = re.compile("")
 response = request.urlopen("")
 for url,name in p.findall(response):
@@ -12,7 +10,27 @@ for url,name in p.findall(response):
 
 
 
+from html.parser import HTMLParser
 
+class Scraper(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        pass
+    def handle_startendtag(self, tag, attrs):
+        pass
+    def handle_endtag(self, tag):
+        pass
+    def handle_data(self, data):
+        pass
+    def handle_charref(self, name):
+        pass
+    def handle_entityref(self, name):
+        pass
+    def handle_comment(self, data):
+        pass
+    def handle_decl(self, decl):
+        pass
+    def handle_pi(self, data):
+        pass
 
 
 
